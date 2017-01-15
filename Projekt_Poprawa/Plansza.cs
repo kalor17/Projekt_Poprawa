26b1df75eac21e961bf93ddc027f94a9b6389cdb
kalor17 <wisniak17@gmail.com>
2017-01-15 22:47:32 +0100

    dodanie klasy plansza i intrfejs ustawienia planszy

---
+++ Projekt_Poprawa/Plansza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Poprawa
+{
+    class Plansza : IUstawieniaPlanszy
+    {
+        bool CzyWygrana();
+        void WstawZnak(int indeks, int znak);
+        int[] ReturnTablica();
+    }
+}
