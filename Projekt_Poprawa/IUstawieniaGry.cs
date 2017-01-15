3c795bfdd0778e835b7a431086374119cd8e379f
kalor17 <wisniak17@gmail.com>
2017-01-15 22:52:38 +0100

    interfejs ustawienia gry

---
+++ Projekt_Poprawa/IUstawieniaGry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_Poprawa
+{
+    interface IUstawieniaGry
+    {
+        void DodajGracza(string nazwa, char znak);
+        void DodajKomputer(string nazwa);
+        void WykonajRuchGracza(int indeks);
+        int WykonajRuchKomputera();
+        bool CzyWygrana();
+        bool CzyRemis();
+    }
+}
